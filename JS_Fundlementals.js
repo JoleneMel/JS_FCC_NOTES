8ea8b3a0f8b6b548c0ee78a6ddcedd0d80a853e0
JoleneMel <jolenemelanson33@gmail.com>
2022-02-09 14:54:33 -0500

    some basic notes, rewatching beginning stuff

---
+++ JS_Fundlementals.js
@@ -0,0 +1,25 @@
+/*All js variables are case sensitive, new variables should be in camelCase, snake case = snake_case(ruby and rust use these), 
+let should be used in place of var when possible 
+
+The difference between var and let keywords, let (ES6 addition) we can declare variable more than once with var(same case etc) while let 
+you can only define once
+Variables with let are still mutable though
+
+const= constant which means it cannot be changed or immuteable
+Common practice with const variables are to name it like below 
+*/
+const FAV_VEGGIE = "Carrots";
+
+/*The above is referred to as SCREAMING_SNAKE_CASE
+
+assignment operators 
++
+-
+>
+<
+>=
+<=
++=
+-= 
+*
+*/
