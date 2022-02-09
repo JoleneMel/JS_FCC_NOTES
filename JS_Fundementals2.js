455f3eac6db7ac401f2e042d2fc8c84544c9209c
JoleneMel <jolenemelanson33@gmail.com>
2022-02-09 15:19:10 -0500

    Preparing for next class to rewatch and note

---
+++ JS_Fundementals2.js
@@ -0,0 +1,3 @@
+/*Class 2 for JS fundmentals (to noteate after, I am going through the videos again to do better notes and make my github more active as I am not able 
+    to post as much so easily)
+*/
