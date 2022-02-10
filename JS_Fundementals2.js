cb268faad89fcc7a7ff10a180169dab02b03d55c
JoleneMel <jolenemelanson33@gmail.com>
2022-02-10 13:21:02 -0500

    notes added

--- JS_Fundementals2.js
+++ JS_Fundementals2.js
@@ -60,3 +60,65 @@ let c = 33;
 a /= 12;
 b /= 4;
 c /= 11;
+
+//Team patpat 
+
+/*
+Escaping Literal Quotes in Strings
+When you are defining a string you must start and end with a single or double quote. 
+What happens when you need a literal quote: " or ' inside of your string?
+
+In JavaScript, you can escape a quote from considering it as an end of string quote by placing a backslash (\) in front of the quote.
+example below 
+*/
+const sampleStr = "Alan said, \"Peter is learning JavaScript\".";
+/*
+This signals to JavaScript that the following quote is not the end of the string, 
+but should instead appear inside the string. So if you were to print this to the console, you would get:
+
+Alan said, "Peter is learning JavaScript".
+
+Use backslashes to assign a string to the myStr variable so that if you were to print it to the console, you would see:
+
+I am a "double quoted" string inside "double quotes".
+*/
+//Basically when you use "" you cannot use "" again within that, why because javascript thinks that you would like to end that line there
+//Then there are missing semi colons and it is a mess. 
+//Using the \ to \"escape"\ it will allow it to used properly :) 
+
+//example 2
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; 
+
+/*
+Quoting Strings with Single Quotes
+String values in JavaScript may be written with single or double quotes, as long as you start and end with the same type of quote. 
+Unlike some other programming languages, single and double quotes work the same in JavaScript.
+examples below 
+*/
+
+const doubleQuoteStr = "This is a string"; 
+const singleQuoteStr = 'This is also a string';
+
+/*
+The reason why you might want to use one type of quote over the other is if you want to use both in a string. 
+This might happen if you want to save a conversation in a string and have the conversation in quotes. 
+Another use for it would be saving an <a> tag with various attributes in quotes, all within a string.
+examples below 
+*/
+
+const conversation = 'Finn exclaims to Jake, "Algebraic!"';
+
+/*
+However, this becomes a problem if you need to use the outermost quotes within it. Remember, a string has the same kind of quote at the beginning and end. 
+But if you have that same quote somewhere in the middle, the string will stop early and throw an error.
+
+example within notes so it will not cause any errors
+const goodStr = 'Jake asks Finn, "Hey, let\'s go on an adventure?"'; 
+const badStr = 'Finn responds, "Let's go!"';
+
+Here badStr will throw an error.
+
+In the goodStr above, you can use both quotes safely by using the backslash \ as an escape character.
+
+Note: The backslash \ should not be confused with the forward slash /. They do not do the same thing.
+*/
