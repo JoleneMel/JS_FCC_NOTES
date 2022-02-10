eed7abe0f35265a8b3f732323ba640631852aa88
JoleneMel <jolenemelanson33@gmail.com>
2022-02-10 12:58:04 -0500

    Starting notes for Class 2!

--- JS_Fundementals2.js
+++ JS_Fundementals2.js
@@ -1,3 +1,20 @@
 /*Class 2 for JS fundmentals (to noteate after, I am going through the videos again to do better notes and make my github more active as I am not able 
     to post as much so easily)
+
+    Remainders in JavaScript using the modulud operator 
+    The remainder operator % gives the remainder of the division of two numbers.
+
+Example
 */
+5 % 2 // = 1 //because
+Math.floor(5 / 2) = 2 //(Quotient)
+2 * 2 //= 4
+5 - 4 //= 1 //(Remainder)
+
+/*
+Usage
+In mathematics, a number can be checked to be even or odd by checking the remainder of the division of the number by 2.
+*/
+17 % 2// = 1 (17 is Odd) So if it has a remainder it is odd 
+48 % 2// = 0 (48 is Even) If not it is even 
+
