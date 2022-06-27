66f085e920f3aa6bbe88be84fba52ad39b8a98df
JoleneMel <jolenemelanson33@gmail.com>
2022-06-27 12:33:08 -0400

    doing it

--- Debugging.js
+++ Debugging.js
@@ -352,4 +352,19 @@ function countToFive() {
   
   countToFive();
   */
- 
+  function countToFive() {
+    let firstFive = "12345";
+    let len = firstFive.length;
+    // Only change code below this line
+    //let i = 0, get rid of =
+    for (let i = 0; i < len; i++) {
+    // Only change code above this line
+      console.log(firstFive[i]);
+    }
+  }
+  
+  countToFive();
+
+  //Debugging Question 10
+  /*
+  */
