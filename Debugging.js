498e0c360adcb7dcc9c63db3a2a8629d998a9dd4
JoleneMel <jolenemelanson33@gmail.com>
2022-06-27 12:19:41 -0400

    doing it

--- Debugging.js
+++ Debugging.js
@@ -223,3 +223,53 @@ if(x === y) {
 }
 
 console.log(result);
+
+//Debugging Question 7
+
+/*
+Catch Missing Open and Closing Parenthesis After a Function Call
+When a function or method doesn't take any arguments, you may forget to include the (empty) opening and closing parentheses when calling it. 
+Often times the result of a function call is saved in a variable for other use in your code. This error can be detected by logging variable values 
+(or their types) to the console and seeing that one is set to a function reference, instead of the expected value the function returns.
+
+The variables in the following example are different:
+
+function myFunction() {
+  return "You rock!";
+}
+let varOne = myFunction;
+let varTwo = myFunction();
+Here varOne is the function myFunction, and varTwo is the string You rock!.
+
+Fix the code so the variable result is set to the value returned from calling the function getNine.
+*/
+
+//Before 
+// function getNine() {
+//     let x = 6;
+//     let y = 3;
+//     return x + y;
+//   }
+  
+//   let result = getNine;//needs () else it will print out what the function is 
+
+//   console.log(result);
+
+//After
+function getNine() {
+    let x = 6;
+    let y = 3;
+    return x + y;
+  }
+  
+  let result2 = getNine();
+  console.log(result2);
+
+  function getNine() {
+    let x = 6;
+    let y = 3;
+    return x + y;
+  }
+  //this works cause result 3 is holding the variable ^^
+  let result3 = getNine;
+  console.log(result3());
